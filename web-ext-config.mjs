741733ba4320a9b112021b142e01298e2fa35f24
FrostyBiscuit <frostybiscuit@proton.me>
2025-03-18 18:33:42 +0100

    Added some exceptions for the web-ext build

--- web-ext-config.mjs
+++ web-ext-config.mjs
@@ -1,20 +1,15 @@
 import "dotenv/config";
 
 export default {
-    artifactsDir: "builds",
-    ignoreFiles: [
-        "node_modules/",
-        ".git/",
-        ".gitignore",
-        ".env"
-    ],
-    build: {
-        overwriteDest: true
-    },
-    sign: {
-        apiKey: process.env.FIREFOX_API_KEY,
-        apiSecret: process.env.FIREFOX_API_SECRET,
-        channel: "listed",
-        amoMetadata: "metadata.json"
-    }
+	artifactsDir: "builds",
+	ignoreFiles: ["node_modules/", "scripts/", "builds/", ".github/", ".git/", ".gitignore", ".env"],
+	build: {
+		overwriteDest: true,
+	},
+	sign: {
+		apiKey: process.env.FIREFOX_API_KEY,
+		apiSecret: process.env.FIREFOX_API_SECRET,
+		channel: "listed",
+		amoMetadata: "metadata.json",
+	},
 };
